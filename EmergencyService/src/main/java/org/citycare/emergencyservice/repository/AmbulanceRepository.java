5ba77b92d16e4c195cba8a476705fdb01b0489ef
H <2478344@cognizant.com>
2026-05-20 10:26:35 +0530

    feat: add EmergencyRepository (findByStatus, findByReportedByUserId, findByDispatchedById), AmbulanceRepository (findByStatus, existsByVehicleNumber), JwtFilter, JwtProvider, SecurityConfig

---
+++ EmergencyService/src/main/java/org/citycare/emergencyservice/repository/AmbulanceRepository.java
@@ -0,0 +1,13 @@
+package org.citycare.emergencyservice.repository;
+
+import org.citycare.emergencyservice.entity.Ambulance;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface AmbulanceRepository extends JpaRepository<Ambulance, Long> {
+    List<Ambulance> findByStatus(Ambulance.Status status);
+    List<Ambulance> findByFacilityId(Long facilityId);
+    List<Ambulance> findByFacilityIdAndStatus(Long facilityId, Ambulance.Status status);
+}
